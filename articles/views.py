448abddfb54b5ac3deb0243e86c95fdaa39eb35a
riumr <atrium_grate.0j@icloud.com>
2022-11-29 17:30:18 +0900

    ADD : API 구성 페이지 생성 [김태형]

--- articles/views.py
+++ articles/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'articles/index.html')
+    return render(request, "articles/index.html")
+
+
+def location_get(request):
+    user_location = request.session.get("userLocation", "none")
+    context = {
+        "userLocation": user_location,
+    }
+    return render(request, "articles/locations.html", context)
