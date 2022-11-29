80b3c5693d2e13bc7f43b7cd6186be367f83823a
unboxing96 <unboxing96@gmail.com>
2022-11-29 16:39:59 +0900

    feat : articles CRUD [김태현]

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
+from .forms import ArticleForm
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
+# Create your views here.
 def index(request):
-    return render(request, 'articles/index.html')
+    articles = Article.objects.all().order_by("-pk")
+    context = {
+        "articles": articles,
+    }
+    return render(request, "articles/index.html", context)
+
+
+def create(request):
+    if request.method == "POST":
+        form = ArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("articles:index")
+    else:
+        form = ArticleForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "articles/create.html", context)
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        "article": article,
+    }
+    return render(request, "articles/detail.html", context)
+
+
+def delete(request, pk):
+    Article.objects.get(pk=pk).delete()
+    return redirect("articles:index")
+
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == "POST":
+        form = ArticleForm(request.POST, request.FILES, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect("articles:detail", article.pk)
+    else:
+        form = ArticleForm(instance=article)
+
+    context = {
+        "form": form,
+    }
+    return render(request, "articles/update.html", context)
--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = "main"
+app_name = "base"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.base, name="base"),
 ]
