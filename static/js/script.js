acfefb9b2f5dc8cc9e1f2d62f67e29f79e2c2076
yoosoonil <y2k7654@gmail.com>
2022-12-03 22:18:05 +0900

    fix : 네비게이션 css 수정[유순일]

--- static/js/script.js
+++ static/js/script.js
@@ -1,56 +1,27 @@
 // 네비게이션 햄버거
-const burger = document.querySelector(".burger");
-const nav = document.querySelector(".nav-links");
-const navlinks = document.querySelectorAll(".nav-links li");
+const toggleBtn = document.querySelector('.navbar__togleBtn');
+const links = document.querySelector('.navbar__links');
 
-const navAnimation = () => {
-  navlinks.forEach((link, index) => {
-    // 애니메이션이 있을 때
-    if (link.style.animation) {
-      // 애니메이션 비움
-      link.style.animation = "";
-    } else {
-      // 애니메이션 없을 때 애니메이션을 추가
-      // 딜레이 간격을 줘서 li가 하나씩 차례대로 나타나도록 설정
-      link.style.animation = `navLinkFade 0.5s ease forwards ${
-        index / 7 + 0.5
-      }s`;
-    }
-  });
-};
-const handleNav = () => {
-  nav.classList.toggle("nav-active");
-
-  //nav Animation
-  navAnimation();
-
-  //burger Animation
-  burger.classList.toggle("toggle");
-};
-const navSlide = () => {
-  burger.addEventListener("click", handleNav);
-};
-
-const setNavTransition = (width) => {
-  if (width > 768) {
-    nav.style.transition = "";
-  } else {
-    nav.style.transition = "transform 0.5s ease-in";
-  }
-};
-
-const handleResize = () => {
-  const width = event.target.innerWidth;
-  setNavTransition(width);
-};
-
-const init = () => {
-  // Toggle Nav
-  window.addEventListener("resize", handleResize);
-  navSlide();
-};
-
-init();
-
+toggleBtn.addEventListener('click', () => {
+    links.classList.toggle('active');
+});
 
 // 마이페이지 라디오버튼
+document.getElementById('radioform').addEventListener("click",  function() {
+  var check_id = Array.from(radioform).find(radio => radio.checked).getAttribute('id').replace('radio', '');
+  console.log(check_id)
+  var radios = document.getElementsByName('menu');
+  for (var i=1; i<=radios.length; i++)
+  {
+      // 체크된 라디오가 현재 인덱스의 id와 같다면
+      // -> show
+      if (check_id === String(i)) {
+      document.querySelector('#cab'+String(i)).setAttribute('style', 'display: block')
+      }
+      // 다르다면
+      // -> hide
+      else {
+      document.querySelector('#cab'+String(i)).setAttribute('style', 'display: none')
+      }
+  }
+  })
