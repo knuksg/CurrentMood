d6aa2958afe4a0ab84aa7608017dd2b3a31af683
soohofather <enthchal@gmail.com>
2022-12-05 16:20:57 +0900

    fix : 배포 오류 수정 [이동현]

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,6 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
@@ -138,6 +137,8 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.User"
+
+DEBUG = os.getenv("DEBUG") == "False"
 
 if DEBUG:
     MEDIA_ROOT = BASE_DIR / "images"
