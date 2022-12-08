f8e2fced02fd259badfb0562be031027e658db40
riumr <atrium_grate.0j@icloud.com>
2022-12-08 18:09:18 +0900

    update : 마커 위치정보 전송시 텍스트 표시 변경 [김태형]

--- static/js/location.js
+++ static/js/location.js
@@ -2,16 +2,30 @@
 // https://developers.google.com/maps/documentation/javascript/geolocation
 let map, infoWindow;
 function initMap() {
-    const coords = localStorage.getItem('crds').split(",").map(Number)
-    const userCoords = { lat: coords[0], lng: coords[1] };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: userCoords,
-    });
-    // 위치 정보 동기화 : 지연시간 짧도록 수정
-    infoWindow = new google.maps.InfoWindow();
-    const locationButton = document.createElement("button");
+  const coords = localStorage.getItem('crds').split(",").map(Number)
+  const userCoords = { lat: coords[0], lng: coords[1] };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: userCoords,
+  });
+  // // 장소 검색
+  // const searchedPlace = document.querySelector("#pac-input")
+  // const searchButton = document.querySelector("#searchButton")
+  // var request = {
+  //   query : "인천",
+  //   fields: ['name', 'geometry'],
+  // };
+  
+  // var service = new google.maps.places.PlacesService(map);
+  // service.findPlaceFromQuery(request, function(results, status) {
+  // if (status === google.maps.places.PlacesServiceStatus.OK) {
+  //   map.setCenter(results[0].geometry.location);
+  // }
+  // });
 
+  // 위치 정보 동기화 : 지연시간 짧도록 수정
+  infoWindow = new google.maps.InfoWindow();
+  const locationButton = document.createElement("button");
     locationButton.textContent = "+";
     locationButton.classList.add("custom-map-control-button");
     map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
@@ -23,23 +37,18 @@ function initMap() {
         lat : 37.3311,
         lng : 126.5811,
       };
-      infoWindow.setPosition(pos);
-      infoWindow.setContent("현재위치");
-      infoWindow.open(map);
       map.setCenter(pos);
       if (navigator.geolocation==false){
         handleLocationError(false, infoWindow, map.getCenter());
       }
     })
     
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      infoWindow.setPosition(pos);
-      infoWindow.setContent(
+    function handleLocationError(browserHasGeolocation) {
+      console.log(
         browserHasGeolocation
           ? "Error: The Geolocation service failed."
           : "Error: Your browser doesn't support geolocation."
       );
-      infoWindow.open(map);
     }
     // 지도 클릭시 좌표 반환
     const marker = new google.maps.Marker({
@@ -49,15 +58,16 @@ function initMap() {
     map.addListener("click", (mapsMouseEvent) => {
       // Create a new InfoWindow.
       const clickedCoordsJson = mapsMouseEvent.latLng.toJSON()
-      const clickedCoords = {lat:Number(clickedCoordsJson['lat']),lng:Number(clickedCoordsJson['lng'])}
+      const clickedCoords = {lat:parseFloat(clickedCoordsJson['lat']),lng:parseFloat(clickedCoordsJson['lng'])}
       const marker = new google.maps.Marker({
         position: clickedCoords,
         map: map,
       });
-      const markerCoords = [clickedCoords['lat'], clickedCoords['lng']]
       // Input창에 위치 넣기
-      const exampleInput = document.querySelector("#exampleInput")
-      exampleInput.value = markerCoords
+      const exampleInput = document.querySelector("#clicked")
+      const geocoder = new google.maps.Geocoder();
+      geocoder.geocode({location:clickedCoords})
+      .then((response)=>exampleInput.value = response.results[0].formatted_address.split(" ").slice(3,5).join(" "))
     });
     
 }
